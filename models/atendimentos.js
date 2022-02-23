b14e356e7937040a21337b1029c60326ba5ceba3
Lucas Araújo <ramaolucas01@gmail.com>
2022-02-23 14:46:58 -0300

    Embelezando

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 class tipes {
-    down(atendimento){
+    down(atendimento) {
         var servidor = atendimento.servidor
         var erro = atendimento.erro
         sendMessage(servidor, erro)
@@ -12,7 +12,7 @@ module.exports = new tipes
 
 
 
-function sendMessage(servidor, erro){
+function sendMessage(servidor, erro) {
     var host = 'http://api.spacewebso.com.br:2121/api'
     var number = '120363041666717455'
     var message = `Servidor ${servidor} parrou de funcionar!\n Erro: ${erro}`
@@ -23,31 +23,31 @@ function sendMessage(servidor, erro){
 
     var axios = require('axios');
     var data = JSON.stringify({
-    "phone": number,
-    "message": message,
-    "isGroup": isGroup
+        "phone": number,
+        "message": message,
+        "isGroup": isGroup
     });
 
     var config = {
-    method: 'post',
-    url: `${host}/${session}/send-message`,
-    headers: { 
-        'Content-Type': 'application/json', 
-        'Authorization': `Bearer ${sessionkey}`
-    },
-    data : data
+        method: 'post',
+        url: `${host}/${session}/send-message`,
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${sessionkey}`
+        },
+        data: data
     };
 
     axios(config)
-    .then(function (response) {
-    if (response.data.status == 'success'){
-        console.log('Mensagem Enviada -', `Servidor: ${servidor} Erro: ${erro}`)
-    } else {
-        console.log('Erro: response-not-success')
-    }
-    })
-    .catch(function (error) {
-    console.log('Erro: catch-function-error', error);
-    });
+        .then(function (response) {
+            if (response.data.status == 'success') {
+                console.log('Mensagem Enviada -', `Servidor: ${servidor} Erro: ${erro}`)
+            } else {
+                console.log('Erro: response-not-success')
+            }
+        })
+        .catch(function (error) {
+            console.log('Erro: catch-function-error', error);
+        });
 
 }
